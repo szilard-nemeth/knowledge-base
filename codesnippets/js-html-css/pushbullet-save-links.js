3faa1cda168512e430c443c7a491e323118b05e1
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-07-21 08:26:31 +0200

    Create pushbullet save links JS snippet

---
+++ codesnippets/js-html-css/pushbullet-save-links.js
@@ -0,0 +1,13 @@
+var script = document.createElement('script');script.src = "https://code.jquery.com/jquery-3.4.1.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+$$ = null;$ = null;$= jQuery;
+
+copy($(".pushbubble").map(function() {
+	var e = $(this),
+	textPart = e.find(".text-part")
+	divs = textPart.children()
+	title = $(divs[0]).text()
+	link = $(divs[1]).text()
+	result = `${title}: ${link}`
+	//console.log(result)
+	return result
+}).get().join("\n"))
